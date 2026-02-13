e855a739b11a4e1898e4a232b50c72f229569ad9
22dce005 <22dce005@charusat.edu.in>
2026-02-13 11:37:16 +0530

    base repair

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Must match GitHub Pages project path: https://username.github.io/Jal-Seva/
-  base: '/Jal-Seva/',
+  base: '',
 })
